db918c078dd6f44596668756e05d6eaa6afa9451
Alexus <77337386+Otakubuns@users.noreply.github.com>
2023-11-26 22:13:32 -0500

    added password reset/sessions and finished video tutorial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+  def set_current_user
+    if session[:userid].present?
+      Current.user = User.find_by(id: session[:userid])
+    end
+  end
+
+  def require_user_logged_in!
+    redirect_to sign_in_path, alert: "You must be signed in" if Current.user.nil?
+  end
 end
